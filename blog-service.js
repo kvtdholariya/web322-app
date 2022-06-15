01e14a3816757891903062df5b05d4f7b444a539
kvtdholariya <kvtdholariya778@gmail.com>
2022-06-15 09:47:18 -0400

    modified blog-service.html and addPost.html

--- blog-service.js
+++ blog-service.js
@@ -1,32 +1,31 @@
 const res = require('express/lib/response');
-const fs = require("fs"); 
+const fs = require("fs");
 var posts = [];
 var categories = [];
 
 module.exports.initialize = () => {
-    return new Promise ((resolve, reject) => {
-        fs.readFile('./data/posts.json', (err,data) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./data/posts.json', (err, data) => {
             if (err) {
-                reject ('unable to read file');
+                reject('unable to read file');
             }
             else {
                 posts = JSON.parse(data);
             }
         });
-
-        fs.readFile('./data/categories.json', (err,data)=> {
+        fs.readFile('./data/categories.json', (err, data) => {
             if (err) {
-                reject ('unable to read file');
+                reject('unable to read file');
             }
             else {
                 categories = JSON.parse(data);
             }
         })
-        resolve("File read.");
+        resolve("My File successfully read.");
     })
 };
 module.exports.getAllPosts = () => {
-    return new Promise ((resolve,reject) => {
+    return new Promise((resolve, reject) => {
         if (posts.length === 0) {
             reject('no results returned');
         }
@@ -36,7 +35,7 @@ module.exports.getAllPosts = () => {
     })
 };
 module.exports.getPublishedPosts = () => {
-    return new Promise ((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         var publishPost = posts.filter(post => post.published == true);
         if (publishPost.length === 0) {
             reject('no results returned');
@@ -45,54 +44,50 @@ module.exports.getPublishedPosts = () => {
     })
 };
 module.exports.getCategories = () => {
-    return new Promise((resolve,reject) => {
+    return new Promise((resolve, reject) => {
         if (categories.length === 0) {
-            reject ('no results returned');
+            reject('no results returned');
         }
         else {
-            resolve (categories);
+            resolve(categories);
         }
     })
 };
 
 module.exports.addPost = (postData) => {
     return new Promise((resolve, reject) => {
-        if(!postData.published) postData.published  = false;//If postData.published is undefined, explicitly set it to false,
+        if (!postData.published) postData.published = false;//If postData.published is undefined, explicitly set it to false,
         postData.id = posts.length + 1;
         posts.push(postData);
         resolve(postData);
     })
 };
-
-
-module.exports.getPostsByCategory = (category) =>{
+module.exports.getPostsByCategory = (category) => {
     let ans = [];
     return new Promise((resolve, reject) => {
         posts.forEach(post => {
-            if(post.category == category){
+            if (post.category == category) {
                 ans.push(post);
             }
         });
         resolve(ans);
     })
 }
-
 module.exports.getPostsByMinDate = (minDateStr) => {
     let ans = [];
     return new Promise((resolve, reject) => {
-        posts.forEach(post => {     
-            if(new Date(post.postDate) >= new Date(minDateStr)){
+        posts.forEach(post => {
+            if (new Date(post.postDate) >= new Date(minDateStr)) {
                 ans.push(post);
             }
         });
         resolve(ans);
     })
 }
-
 module.exports.getPostById = (id) => {
     return new Promise((resolve, reject) => {
-        posts.forEach(post => {     
-            if(post.id == id)
+        posts.forEach(post => {
+            if (post.id == id)
                 resolve(post);
         });
     })
