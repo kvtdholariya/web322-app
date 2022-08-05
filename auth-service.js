3d69c6d849e5f99c70224b6a257f129be9dc96f4
Kavita Dholariya <98116118+kvtdholariya@users.noreply.github.com>
2022-08-04 22:53:10 -0400

    modified

--- auth-service.js
+++ auth-service.js
@@ -19,21 +19,6 @@ var userSchema = new Schema({
 
 let User; // to be defined on new connection 
 
-/*
-module.exports.initialize = function () {
-return new Promise(function (resolve, reject) {
-let db = mongoose.createConnection("connectionString");
-db.on('error', (err)=>{
-reject(err); // reject the promise with the provided error
-});
-db.once('open', ()=>{
-User = db.model("users", userSchema);
-resolve();
-});
-});
-};
-*/ 
-
 module.exports.initialize = function () {
     return new Promise(function (resolve, reject) {
        let db = mongoose.createConnection("mongodb+srv://MongoDBUser:Coop2022$$@senecaweb.ojaonah.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true});
@@ -47,60 +32,6 @@ module.exports.initialize = function () {
     });
 };
 
-/*
-module.exports.registerUser = function (userData) {
-    return new Promise((resolve, reject) => {
-        if (userData.password !== userData.password2)
-            reject("Passwords do not match");
-
-        bcrypt.hash(userData.password, 10).then(hash => {
-            userData.password = hash;
-            let newUser = new User(userData);
-
-            newUser.save((err) => {
-                if (err && err.code === 11000)
-                    reject("User Name already taken");
-                else if (err && err.code !== 11000)
-                    reject("There was an error creating the user: " + err);
-                else
-                    resolve();
-            });
-        
-        }).catch(err=>{
-            reject(err);
-        });
-
-    });
-}
-
-module.exports.checkUser = (userData) => {
-    return new Promise((resolve, reject) => {
-        User.find({ userName: userData.userName })
-            .exec()
-            .then(users => {
-                bcrypt.compare(userData.password, users[0].password).then(res => {
-                    if (res === true) {
-                        users[0].loginHistory.push({ dateTime: (new Date()).toString(), userAgent: userData.userAgent });
-                        User.update(
-                            { userName: users[0].userName },
-                            { $set: { loginHistory: users[0].loginHistory } },
-                            { multi: false }
-                        )
-                            .exec()
-                            .then(() => { resolve(users[0]) })
-                            .catch(err => { reject("There was an error verifying the user: " + err) })
-                    }
-                    else {
-                        reject("Incorrect Password for user: " + userData.userName);
-                    }
-                })
-            })
-            .catch(() => {
-                reject("Unable to find user: " + userData.userName);
-            })
-    })
-};
-*/
 module.exports.registerUser = (userData) => {
     return new Promise((resolve, reject) => {
         if (userData.password != userData.password2) {
